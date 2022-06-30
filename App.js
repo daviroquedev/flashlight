4a76f582b8b41ac8e3e25a6e93c585d80d108b77
Davi Roque <sowdavi@gmail.com>
2022-06-30 18:06:36 -0300

    torch and RNShake add

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
-import React from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake'
 
 
 const App = () => {
 
-  const toggle = true; //false
+  const [toggle, setToggle] = useState(false);
 
-  
+  const handleChangleToggle = () => setToggle(oldToggle => !oldToggle)
+
+  useEffect(()=>{
+  //liga 
+  Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect( ()=>{
+    /** 
+     * quando o  celular for chacoalhado mudaremos o toggle
+     * 
+    */
+    const subscription = RNShake.addListener(()=>{
+      handleChangleToggle(oldToggle => !oldToggle);
+    });
+
+    // ESSA FUNÇÃO VAI SER CHAMADA QUANDO O COMPONENTS
+    // FOR SER DESMONTADO
+    return () => subscription.remove();
+  },[])
+
   return (
-  
-  <View style={toggle ? styles.containerLight : styles.container} >
-    <Image
-      source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png')}
-      style={toggle ? styles.lightingOn : styles.lightingOff}
-    />
-
-    <Image
-      source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png')}
-      style={styles.dioLogo}
-    />
 
 
 
-  </View>
-)}
+    <View style={toggle ? styles.containerLight : styles.container} >
+      <TouchableOpacity onPress={handleChangleToggle}>
+        <Image
+          source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png')}
+          style={toggle ? styles.lightingOn : styles.lightingOff}
+        />
+        <Image
+          source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png')}
+          style={styles.dioLogo}
+        />
+      </TouchableOpacity>
+
+
+    </View>
+  )
+}
 
 
 
